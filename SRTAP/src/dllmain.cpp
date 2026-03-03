16737712b314224bd6038ece928a5a3b05dffb03
Andrew Davis <adavis@azure-drake.com>
2026-03-02 20:05:04 -0500

    Mod setup
    reorganized a little and converted to represent the mod being made

--- SRTAP/src/dllmain.cpp
+++ SRTAP/src/dllmain.cpp
@@ -3,7 +3,7 @@
 #include <Unreal/UObjectGlobals.hpp>
 #include <Unreal/UObject.hpp>
 
-namespace MyMods
+namespace SRTAP
 {
     using namespace RC;
     using namespace Unreal;
@@ -11,24 +11,24 @@ namespace MyMods
     /**
     * MyExampleMod: UE4SS c++ mod class defintion
     */
-    class MyExampleMod : public RC::CppUserModBase {
+    class SpyroRTAPMod : public RC::CppUserModBase {
     public:
         
         // constructor
-        MyExampleMod() {
+        SpyroRTAPMod() {
             ModVersion = STR("0.1");
-            ModName = STR("MyExampleMod");
-            ModAuthors = STR("UE4SS");
-            ModDescription = STR("A basic template C++ mod");
+            ModName = STR("SRTAP");
+            ModAuthors = STR("UE4SS, Curin");
+            ModDescription = STR("adds archipelago support to Spyro: Reignited Trilogy");
             // Do not change this unless you want to target a UE4SS version
             // other than the one you're currently building with somehow.
             //ModIntendedSDKVersion = STR("2.6");
             
-            Output::send<LogLevel::Warning>(STR("[MyExampleMod]: Init.\n"));
+            Output::send<LogLevel::Warning>(STR("[SpyroRTAPMod]: Init.\n"));
         }
         
         // destructor
-        ~MyExampleMod() override {
+        ~SpyroRTAPMod() override {
             // fill when required
         }
 
@@ -54,10 +54,10 @@ namespace MyMods
 * export the start_mod() and uninstall_mod() functions to
 * be used by the core ue4ss system to load in our dll mod
 */
-#define MOD_EXPORT __declspec(dllexport) 
+#define SpyroRTAPMod_EXPORT __declspec(dllexport) 
 extern "C" {
-    MOD_EXPORT RC::CppUserModBase* start_mod(){ return new MyMods::MyExampleMod(); }
-    MOD_EXPORT void uninstall_mod(RC::CppUserModBase* mod) { delete mod; }
+    SpyroRTAPMod_EXPORT RC::CppUserModBase* start_mod(){ return new SRTAP::SpyroRTAPMod(); }
+    SpyroRTAPMod_EXPORT void uninstall_mod(RC::CppUserModBase* mod) { delete mod; }
 }
     
 
